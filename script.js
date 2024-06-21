e35e95e136958f2bc22aaf0d96abf2bcc41a1103
laldrich5 <lgaldrich8@gmail.com>
2024-06-21 16:17:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+msg.text = document.querySelector('[name="text"]').value;
+const utterance = new SpeechSynthesisUtterance();
+utterance.text = document.querySelector('[name="message"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .filter((voice) => voice.lang.includes("en"))
+    .map(
+      (voice) =>
+        `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
+    )
+    .join("");
+}
+
+function setVoice() {
+  msg.voice = voices.find((voice) => voice.name === this.value);
+  toggle();
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+function respond(){
+    speechSynthesis.speak(utterance);
+
+    if (window.addEventListener("keydown", stop) = true ){
+      speechSynthesis.cancel();
+    }
+  }
+
+function stop(){
+  speechSynthesis.cancel();
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
+options.forEach((option) => option.addEventListener("change", setOption));
+window.addEventListener("click", toggle)
+window.addEventListener("keydown", respond)
